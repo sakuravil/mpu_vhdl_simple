c06453f3f2815f06dcd95c37f184ca4d2b7650cf
sakuravil <abc22921@gmail.com>
2017-10-21 23:14:45 +0900

    文字化けしてたのでfix

--- fulladder.vhd
+++ fulladder.vhd
@@ -1,33 +1,5 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    15:23:22 03/18/2014 
--- Design Name: 
--- Module Name:    fulladder - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity fulladder is
 	PORT (A : in std_logic;
@@ -56,13 +28,13 @@ architecture Behavioral of fulladder is
 begin
 	
 	halfadder1: halfadder PORT MAP (
-          A => A,						--‚±‚±‚Æ‚»‚Ì‰º3‚Â‚Í“¯Žž‚ÉŽÀs‚³‚ê‚é
+          A => A,
           B => B,
           O => S1,
           CO => S2
         );
 	halfadder2: halfadder PORT MAP (
-          A => S1,						--‚±‚±‚Æ‚»‚Ì‰º3‚Â‚Í“¯Žž‚ÉŽÀs‚³‚ê‚é
+          A => S1,
           B => X,
           O => S,
           CO => S3
